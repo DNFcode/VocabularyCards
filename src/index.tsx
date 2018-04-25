dd12c4376daf80273510e6e0aca85537f541f0c4
Nikita Vasilev <dnfletter@gmail.com>
2018-04-25 10:43:21 +0300

    Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,20 @@ import { AppContainer } from "react-hot-loader"
 import App from "./App"
 import { initStore } from "./redux/store"
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function(registration) {
+      console.log(
+        "Hooray. Registration successful, scope is:",
+        registration.scope
+      )
+    })
+    .catch(function(err) {
+      console.log("Whoops. Service worker registration failed, error:", err)
+    })
+}
+
 const store = initStore()
 
 const render = (Component: typeof App) => {
