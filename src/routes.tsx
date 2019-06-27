d6a2eb20c6a623cf914d6336f5a271500e9f45c3
Nikita Vasilev <dnfletter@gmail.com>
2019-06-27 20:12:04 +0300

    Routes prototype

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,60 +1,51 @@
-// import CardsPage from "./components/CardsPage"
-// import StatsPage from "./components/StatsPage"
-// import SettingsPage from "./components/SettingsPage"
-// import GlossaryPage from "./components/GlossaryPage"
-// import { NewCardDialog } from "./components/NewCardDialog"
-// import { EditCardDialog } from "./components/EditCardDialog"
+import CardsPage from "./components/CardsPage"
+import StatsPage from "./components/StatsPage"
+import SettingsPage from "./components/SettingsPage"
+import GlossaryPage from "./components/GlossaryPage"
+import { NewCardDialog } from "./components/NewCardDialog"
+import { EditCardDialog } from "./components/EditCardDialog"
+import { computed } from "mobx"
 
-// export enum Position {
-//   Top,
-//   Bottom,
-// }
+class Routing {
+  get currentRoute() {
+    return window.location.pathname
+  }
+}
 
-// class View {
-//   constructor(
-//     private url: string,
-//     private view: React.ReactNode,
-//     public children?: { [route: string]: View }
-//   ) {}
-// }
+const routes = [
+  {
+    path: "/cards",
+    component: CardsPage,
+  },
+  {
+    path: "/stats",
+    component: StatsPage,
+  },
+  {
+    path: "/settings",
+    component: SettingsPage,
+  },
+  {
+    path: "/glossary",
+    component: GlossaryPage,
+    children: [
+      {
+        path: "/glossary/actions/:id",
+        component: EditCardDialog,
+        action: true,
+      },
 
-// class Routing {
-//   learning = StackView(LearningPage, [])
-// }
+      {
+        path: "/glossary/card/:id",
+        component: EditCardDialog,
+      },
+    ],
+  },
+  {
+    path: "/glossary/new",
+    component: NewCardDialog,
+    action: true,
+  },
+]
 
-// const routing = {
-//   cards: CardsPage,
-// }
-
-// export default [
-//   {
-//     path: "/cards",
-//     component: CardsPage,
-//   },
-//   {
-//     path: "/stats",
-//     component: StatsPage,
-//   },
-//   {
-//     path: "/settings",
-//     component: SettingsPage,
-//   },
-//   {
-//     path: "/glossary",
-//     component: GlossaryPage,
-//     children: [
-//       {
-//         path: "/glossary/actions/:id",
-//         component: EditCardDialog,
-//       },
-//       {
-//         path: "/glossary/new",
-//         component: NewCardDialog,
-//       },
-//       {
-//         path: "/glossary/card/:id",
-//         component: EditCardDialog,
-//       },
-//     ],
-//   },
-// ]
+function goTo(from: string, to: string) {}
